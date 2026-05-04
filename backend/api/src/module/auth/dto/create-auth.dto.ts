8fc42f98f44531c5c22c486f5fe42a410e000000
osamamohti2002 <99078852@mydavinci.nl>
2026-05-04 12:01:25 +0200

    feat: add create/login DTOs and install class-validator

--- backend/api/src/module/auth/dto/create-auth.dto.ts
+++ backend/api/src/module/auth/dto/create-auth.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAuthDto {}
+import { Role } from '@prisma/client';
+import { IsEmail, IsString, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  role: Role;
+}
