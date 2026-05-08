b8f8a2c0a2e5a2b7a7717ea2ca0912c37f3f3669
osamamohti2002 <99078852@mydavinci.nl>
2026-05-08 14:47:21 +0200

    feat(users): add UsersModule with getMe endpoint

--- backend/api/src/module/user/user.controller.ts
+++ backend/api/src/module/user/user.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth-guard';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser() user: any){
+    return this.userService.getMe(user.sub);
   }
 }
--- backend/api/src/module/user/user.service.ts
+++ backend/api/src/module/user/user.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private prisma: PrismaService){}
+  async getMe(userId: string){
+    const user = await this.prisma.user.findUnique({
+      where:{
+        id: userId,
+      },
+      select: {
+        name: true,
+        email: true,
+        id: true,
+        role: true,
+        customerProfile: true,
+      }
+    });
 
-  findAll() {
-    return `This action returns all user`;
-  }
+    if (!user){
+      throw new NotFoundException('User not found');
+    }
+    return {
+      message: `welcome ${user?.name}`,
+      user: user
+    };
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }
