3db4376b404dab6881e17c84fa7d4fd2f1795612
osamamohti2002 <99078852@mydavinci.nl>
2026-05-15 15:31:21 +0200

    feat(restaurant): add applyRestaurant endpoint

--- backend/api/src/module/restaurant/restaurant.controller.ts
+++ backend/api/src/module/restaurant/restaurant.controller.ts
@@ -1,34 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { RestaurantService } from './restaurant.service';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth-guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
-  @Post()
-  create(@Body() createRestaurantDto: CreateRestaurantDto) {
-    return this.restaurantService.create(createRestaurantDto);
+  @Post('apply')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.RESTAURANT_OWNER)
+  applyRestaurant(@CurrentUser() user: any, @Body() data: CreateRestaurantDto){
+    return this.restaurantService.applyRestaurant(user.sub, data);
   }
 
-  @Get()
-  findAll() {
-    return this.restaurantService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.restaurantService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRestaurantDto: UpdateRestaurantDto) {
-    return this.restaurantService.update(+id, updateRestaurantDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.restaurantService.remove(+id);
-  }
 }
--- backend/api/src/module/restaurant/restaurant.service.ts
+++ backend/api/src/module/restaurant/restaurant.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException,  } from '@nestjs/common';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dto/update-restaurant.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class RestaurantService {
-  create(createRestaurantDto: CreateRestaurantDto) {
-    return 'This action adds a new restaurant';
+
+  constructor(private readonly prisma: PrismaService) { }
+
+  async applyRestaurant(userId: string, data: CreateRestaurantDto) {
+    const restaurantOwner = await this.prisma.user.findUnique({
+      where:{
+        id:userId
+      }
+    });
+
+    if(!restaurantOwner){
+      throw new NotFoundException("User Not Found ");
+    };
+
+    const existingRestaurant = await this.prisma.restaurant.findUnique({
+      where:{
+        userId:userId
+      }
+    })
+
+    if(existingRestaurant){
+      throw new BadRequestException("You already have a restaurant");
+    };
+
+    const restaurant = await this.prisma.restaurant.create({
+      data:{
+        userId:userId,
+        name:data.name,
+        address:data.address,
+        phoneNumber:data.phoneNumber,
+        logoUrl:data.logoUrl,
+        headerImageUrl:data.headerImageUrl,
+        deliveryTimeMin:data.deliveryTimeMin,
+        deliveryTimeMax:data.deliveryTimeMax
+      }
+    })
+    
+    return {
+      message:"Restaurant Created Successfully",
+      data: restaurant
+    };
+
   }
 
-  findAll() {
-    return `This action returns all restaurant`;
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} restaurant`;
-  }
 
-  update(id: number, updateRestaurantDto: UpdateRestaurantDto) {
-    return `This action updates a #${id} restaurant`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} restaurant`;
-  }
 }
